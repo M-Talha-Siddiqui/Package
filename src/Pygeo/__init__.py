66773664005510e2244ff56cb9f61080580d123f
M Talha <talhasiddiqui01@gmail.com>
2020-07-27 06:11:07 +0200

    Refactor package to use src layout

--- src/Pygeo/__init__.py
+++ src/Pygeo/__init__.py
@@ -2,4 +2,4 @@
 
 # first it was only the above the print statement but if I change something in it such as the print command below it will be auto updated.
 
-print(f"Hello  {__name__}! , Hey It's a nice day ,isn't it ? ")
+print(f"Hello  {__name__}! , Hey ,It's a nice day ,isn't it ? ")
