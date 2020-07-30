7d2b6bb73128111c5a1c61ffd86bc5d801b74c31
M Talha <talhasiddiqui01@gmail.com>
2020-07-30 17:47:09 +0200

    Refactor package to use src-layout

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = "PyGeo", 
     version = "0.1.0",
-    package_dir={" ": "src"},
+    package_dir={"": "src"},
     packages = find_packages(where= "src"),
     
 )
