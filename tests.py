81dcd570f2834c8f59522a536be0482869c1b867
M Talha <talhasiddiqui01@gmail.com>
2020-07-30 17:27:40 +0200

    Little setback

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import code_to_test
 # you can import from the file such as seen above 
 #but to import from directory is difficult such as in 
 
-import Pygeo
+import pygeo
 # now when we execute it print Hello,World! from __init__ file
 # when we give the command of import Pygoe ,first it will for a file name Pygeo.py
 # if it didnot find it, it will then look for directory Pygeo file and init  will look for __init__.py to execute
